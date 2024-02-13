1b5762e3c9c11e3f6999bf7764d388f55107a776
Himanshu Garg <114793931+Hima93511@users.noreply.github.com>
2024-02-13 12:38:38 +0530

    Add files via upload

---
+++ http modules.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var server=http.createServer(function (req,res)
+{ if(req.url=='/')
+{ res.write('welcome to the index page');
+res.end();
+} else if (req.url== "/emp")
+{ res.write('welcome to the employee page');
+res.end();
+}else if (req.url=="/admin")
+{res.write('welcome to the admin page');
+res.end();
+} else res.eventNames('Invalid Request!'); });
+server.listen(2020);
+console.log('Server is running at port 2020')
