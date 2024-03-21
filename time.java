d25202a1d78be43badc1c19d22e7f4ddbbe599be
Himanshu Garg <114793931+Hima93511@users.noreply.github.com>
2024-03-21 16:40:25 +0530

    Add files via upload

---
+++ time.java
@@ -0,0 +1,21 @@
+import java.time.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        
+        int dd = date.getDayOfMonth();
+        int mm = date.getMonthValue();
+        int yyyy = date.getYear();
+        
+        System.out.printf("%d-%d-%d\n", dd, mm, yyyy);
+        
+        int h = time.getHour();
+        int m = time.getMinute();
+        int s = time.getSecond();
+        int n = time.getNano();
+        
+        System.out.printf("%d:%d:%d:%d\n", h, m, s, n);
+    }
+}
